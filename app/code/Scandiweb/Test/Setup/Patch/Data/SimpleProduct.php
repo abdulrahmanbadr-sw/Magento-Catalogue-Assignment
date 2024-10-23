eaad25d67b1da76d7db26ff1add93f5409395f94
--replace-all <abdulrahman.badr@scandiweb.com>
2024-10-23 12:46:59 +0300

    Making the necessary adjustments

--- app/code/Scandiweb/Test/Setup/Patch/Data/SimpleProduct.php
+++ app/code/Scandiweb/Test/Setup/Patch/Data/SimpleProduct.php
@@ -5,64 +5,165 @@ namespace Scandiweb\Test\Setup\Patch\Data;
 use Magento\Catalog\Api\CategoryLinkManagementInterface;
 use Magento\Catalog\Api\Data\ProductInterfaceFactory;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Catalog\Model\Product\Type;
+use Magento\Catalog\Model\Product\Visibility;
+use Magento\Eav\Setup\EavSetup;
 use Magento\Framework\App\State;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Store\Model\StoreManagerInterface;
 
+/**
+ * Class SimpleProduct
+ * @package Scandiweb\Test\Setup\Patch\Data
+ */
 class SimpleProduct implements DataPatchInterface
 {
-    private $productFactory;
-    private $productRepository;
-    private $categoryLinkManagement;
-    private $storeManager;
-    private $appState;
+    /**
+     * @var ProductInterfaceFactory
+     */
+    protected ProductInterfaceFactory $productFactory;
 
+    /**
+     * @var ProductRepositoryInterface
+     */
+    protected ProductRepositoryInterface $productRepository;
+
+    /**
+     * @var CategoryLinkManagementInterface
+     */
+    protected CategoryLinkManagementInterface $categoryLinkManagement;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    protected StoreManagerInterface $storeManager;
+
+    /**
+     * @var State
+     */
+    protected State $appState;
+
+    /**
+     * @var EavSetup
+     */
+    protected EavSetup $eavSetup;
+
+    /**
+     * SimpleProduct constructor.
+     *
+     * @param ProductInterfaceFactory $productFactory
+     * @param ProductRepositoryInterface $productRepository
+     * @param CategoryLinkManagementInterface $categoryLinkManagement
+     * @param StoreManagerInterface $storeManager
+     * @param State $appState
+     * @param EavSetup $eavSetup
+     */
     public function __construct(
         ProductInterfaceFactory $productFactory,
         ProductRepositoryInterface $productRepository,
         CategoryLinkManagementInterface $categoryLinkManagement,
         StoreManagerInterface $storeManager,
-        State $appState
+        State $appState,
+        EavSetup $eavSetup
     ) {
         $this->productFactory = $productFactory;
         $this->productRepository = $productRepository;
         $this->categoryLinkManagement = $categoryLinkManagement;
         $this->storeManager = $storeManager;
         $this->appState = $appState;
+        $this->eavSetup = $eavSetup;
     }
 
-    public function apply()
+    /**
+     * Apply data patch to create a simple product.
+     *
+     * @return void
+     */
+    public function apply(): void
     {
-        $this->appState->setAreaCode('adminhtml');
+        $this->appState->emulateAreaCode('adminhtml', [$this, 'execute']);
+    }
+
+    /**
+     * Execute the product creation logic.
+     *
+     * @return void
+     */
+    public function execute(): void
+    {
+        string $sku = 'simple-product-sku';
+
+        string $name = 'simple-product-name';
+
+        // Check if the product already exists
+        if ($product->getIdBySku($sku)) {
+            return;
+        }
 
         $product = $this->productFactory->create();
-        $product->setSku('simple-product-sku');
-        $product->setName('Simple Product');
-        $product->setAttributeSetId(4); 
-        $product->setStatus(1);
-        $product->setVisibility(4); 
-        $product->setTypeId('simple');
-        $product->setPrice(10.00);
-        $product->setWebsiteIds([$this->storeManager->getStore()->getWebsiteId()]);
-        $product->setStockData([
-            'use_config_manage_stock' => 1,
-            'qty' => 100,
-            'is_qty_decimal' => 0,
-            'is_in_stock' => 1,
-        ]);
 
+        // Get the attribute set id from EavSetup object
+        $attributeSetId = $this->eavSetup->getAttributeSetId(Product::ENTITY, 'Default');
+
+        // Set attributes
+        $product->setTypeId(Type::TYPE_SIMPLE)
+            ->setAttributeSetId($attributeSetId)
+            ->setName($name)
+            ->setSku($sku)
+            ->setUrlKey('Simple Product')
+            ->setPrice(9.99)
+            ->setVisibility(Visibility::VISIBILITY_BOTH)
+            ->setStatus(Status::STATUS_ENABLED)
+            ->setWebsiteIds([$this->storeManager->getStore()->getWebsiteId()])
+            ->setStockData([
+                'use_config_manage_stock' => 1,
+                'qty' => 100,
+                'is_qty_decimal' => 0,
+                'is_in_stock' => 1,
+            ]);
+        
         $this->productRepository->save($product);
 
+        $sourceItem = $this->sourceItemFactory->create();
+
+        $sourceItem->setSourceCode('default');
+
+        $sourceItem->setQuantity(100);
+
+        $sourceItem->setSku($product->getSku());
+
+        $sourceItem->setStatus(SourceItemInterface::STATUS_IN_STOCK);
+
+        $this->sourceItems[] = $sourceItem;
+
+        $this->sourceItemsSaveInterface->execute($this->sourceItems);
+        
+
         $categoryIds = [2]; 
+
         $this->categoryLinkManagement->assignProductToCategories($product->getSku(), $categoryIds);
     }
 
-    public static function getDependencies()
+    /**
+     * Get dependencies for the data patch.
+     *
+     * @return array
+     */
+    public static function getDependencies(): array
     {
         return [];
     }
 
-    public function getAliases()
+    /**
+     * Get aliases for the data patch.
+     *
+     * @return array
+     */
+    public function getAliases(): array
     {
         return [];
     }
